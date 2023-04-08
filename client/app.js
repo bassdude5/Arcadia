678ace821f4cfe1dd8aa9e59d9f699f29bde6938
K-eli-wtech <kewhite3@my.waketech.edu>
2023-04-08 17:53:15 -0400

    updating app.js

--- client/app.js
+++ client/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 // Serve static files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the index.html file for all requests
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
